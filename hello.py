57ace4e6a11a066bbcd744ca34c2e6953291a142
unknown <kaitlynher23@berkeley.edu>
2024-01-31 11:17:17 -0800

    adding file

---
+++ hello.py
@@ -0,0 +1,4 @@
+print(((64818/6)**(1/3))-16.10623548)
+#'64818' was my lunch number in high school, and there's '6' instructors in this class
+#the solution is the number of letters of an instructor who is on the slides but not in the OH section lolol
+#the name is bc I want to say hi
